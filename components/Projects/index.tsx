ec1c9b16be2e3c3cb75ce65ea9890a9fa4a06077
atul <atkr5496@gmail.com>
2022-10-09 23:37:37 +0530

    ..

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -104,20 +104,7 @@ export function Projects() {
             </div>
 
             <p className="text-gray-500 mt-4">{p.description}</p>
-            {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6 mr-3"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
-                  />
-                </svg> */}
+
             <div className="flex items-center space-x-3">
               {p.appLink ? (
                 <a
